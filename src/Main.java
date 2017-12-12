28370f27eae268814acd4dccc038da9c9f81c075
posusaevkostay@mail.ru <posusaevkostay@mail.ru>
2017-12-12 22:38:24 +0300

    Task2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.pow;
+
+public class Main {
+    public static void main(String[] args) {
+        float a=14;
+        float result = a / 209 +(float) pow(a,  (29 - (float)pow(13, 2) +  a / 3));
+        System.out.println(result);
+    }
+}
